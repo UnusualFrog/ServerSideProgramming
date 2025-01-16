d8c5cc9c87e09d1b13528a955c6407467cfaf9b2
Julia Forward <forwardnoah@gmail.com>
2025-01-16 11:08:55 -0330

    Added mongoDb and mongoose

--- MyApp/app.js
+++ MyApp/app.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const birds = require('./birds')
+
 const app = express()
 const port = 3000
+
+mongoose.connect('mongodb:Student13:Student13@logan',{ dbName: 'home13'})
+    .then(() => {
+      console.log("Connected to the database!");
+      app.listen(port, () => {
+          console.log(`Example app listening on port ${port}`);
+      });
+    })
+    .catch(() => {
+      console.log("Failed to connect to the database.");
+    })
 
 // Default
 //app.get('/', (req, res) => {
@@ -17,12 +30,13 @@ app.get('/', (req, res, next) => {
  res.send('Hello World!')
 })
 
+// Test code remove once mongo db works
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
 app.put('/', (req, res) => {
-    res.send('putting')
+    res.send('putting up with bs')
 })
 
 app.use('/birds', birds)
