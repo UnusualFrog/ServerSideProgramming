2a4ae63e3429826db12160f754713eeef2dbb158
Julia Forward <forwardnoah@gmail.com>
2025-01-21 09:59:11 -0330

    MongoDB setup

--- MyApp/app.js
+++ MyApp/app.js
@@ -5,7 +5,7 @@ const birds = require('./birds')
 const app = express()
 const port = 3000
 
-mongoose.connect('mongodb:Student13:Student13@logan',{ dbName: 'home13'})
+mongoose.connect('mongodb+srv://juliaforward:easypass1@home13.vxafn.mongodb.net/?retryWrites=true&w=majority&appName=home13',{ dbName: 'home13'})
     .then(() => {
       console.log("Connected to the database!");
       app.listen(port, () => {
@@ -16,27 +16,28 @@ mongoose.connect('mongodb:Student13:Student13@logan',{ dbName: 'home13'})
       console.log("Failed to connect to the database.");
     })
 
-// Default
-//app.get('/', (req, res) => {
-//  res.send('Hello World!')
-//})
-
-
-// Chained
+// Chained get request
 app.get('/', (req, res, next) => {
    console.log("test");
    next()
 },(req, res)=>{
- res.send('Hello World!')
+  res.send('Hello Chain!')
 })
 
-// Test code remove once mongo db works
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+// Default put request
 app.put('/', (req, res) => {
     res.send('putting up with bs')
 })
 
+// Use birds.js for routing the birds route
 app.use('/birds', birds)
+
+// // Test code remove once mongo db works
+// app.listen(port, () => {
+//   console.log(`Example app listening on DEFAULT port ${port}`)
+// })
+
+// Default get request
+// app.get('/', (req, res) => {
+//  res.send('Hello World!')
+// })
---
+++ MyApp/models/course.models.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const SessionSubSchema = mongoose.Schema ({
+    day: {
+        type: Number,
+        required: true,
+        default:1
+        },
+    startTime: {
+        type: Number,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    room: {
+        type: String,
+        required: true
+    }
+});
+
+
+const CourseSchema = mongoose.Schema ({
+        courseName: {
+            type: String,
+            required: [true, "Please enter the course name"],
+        },
+        courseTitle: {
+            type: String,
+            required: [true, "Please enter the course title"],
+        },
+        sessions: {
+            type:[SessionSubSchema]
+        },
+    },
+    {
+        timestamps: true
+    });
+
+const Course = mongoose.model("Course", CourseSchema);
+
+module.exports = Course;
