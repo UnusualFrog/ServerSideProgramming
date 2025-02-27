169f09627890f173a84897c390a95810fb6f6d03
Forward <Julia.Forward@k202domain.com>
2025-02-27 09:05:20 -0330

    quiz1

---
+++ quiz/berg.model.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+
+const BergSchema = mongoose.Schema ({
+    mission: {
+        type: String,
+        required: [true, "Please enter the mission ID"],
+    },
+    aircraft: {
+        type: String,
+        required: [true, "Please enter a callsign"],
+        default: "C-GCNA"
+    },
+    latitiude: {
+        type: Double,
+        required: [true, "Please enter a latititude"]
+    },
+    longitude: {
+        type: Double,
+        required: [true, "Please enter a longitude"]
+    },
+    altitude: {
+        type: Double,
+        required: [true, "Please enter an altitude"]
+    },
+    timestamp: {
+        type: String,
+        required: [true, "Please enter a timestamp"]
+    },
+    bergId: {
+        type: Number,
+        required: [true, "Please enter a berg ID"]
+    },
+    iceType: {
+        type: String,
+        default: "Tabular"
+    },
+    iceSize: {
+        type: Number,
+    },
+    seaState: {
+        type: String,
+        default: "Calm"
+    },
+    seaTemp: {
+        type: Double
+    }, 
+    iceGrounded: {
+        type: Boolean,
+        default: false
+    },
+    comment: {
+        type: String
+    }
+
+},
+{ 
+    timestamps: true
+});
+
+const Berg = mongoose.model("berg", BergSchema);
+
+module.exports = Berg;
